57da9a2560cbe5d49c6a391200a38f4a95ae8e53
skirk20 <lukasjanaszek@gmail.com>
2022-08-19 13:55:05 +0200

    poprawki pod wcag

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import logoFirst from './img/krokodylKomp.svg';
-import logoSec from './img/ministerstwoKomp.svg';
+import logoSec from './img/ministerstwoKomp.png';
 import logoSecMob from './img/ministerstwoMobilna.svg';
 const Container = styled.footer`
 
@@ -28,39 +28,80 @@ const Container = styled.footer`
     }
     
     .logo1,.logo2{
-        height: 5em;
-        margin:0 auto;
-        width:50%;
-        height:50%;
+        padding:1em 0em;
     }
     .logo1{
+        margin-left: 6em;
         width:10%;
         text-align: center;
         vertical-align:middle;
+        @media(max-width: 1560px){
+            width:20%;
+            margin:0;
+        }
+        @media(max-width: 950px){
+            padding:0;
+            width:30%;
+            margin:0 auto;
+            margin-bottom: 0.5em;
+        }
         
     }
     .logo2{
         height: auto;
         text-align: center;
         vertical-align:middle;
-        
+        width:40%;
+        margin: 0 auto;
         
         @media (max-width: 1560px) {
-            padding:2.5em;
+            // padding:2.5em;
+            width:70%;
             
         }
         @media(max-width: 950px){
-            // margin:0 auto;
-            // margin-bottom: 0.5em;
+            padding:0;
+            width:100%;
+            height:40%; 
+            margin:0 auto;
+            margin-bottom: 0.5em;
+        }
+        @media(max-width: 750px){
+
         }
     }
-    
+    .mkid{
+        margin-left:10%;
+        display:flex;
+        width:50%;
+        text-align: center;
+        height: 30%;
+        width:70%;
+        p{
+            font-size:16px;
+            margin: auto;
+            text-align: left;
+            margin-left:1em;
+        }
+        @media(max-width: 1560px){
+            display:block;
+            margin:0;
+            margin-bottom: 1em;
+            width:60%;
+            p{
+                width:auto;
+            }
+        }
+    }
     
 `;
 
 export default function PageFooter() {
     return <Container>
         <img className="logo1" src={logoFirst} alt="Brunon Schultz, artysta genialny"/>
+        <div className="mkid">
         <img className="logo2" src={logoSec} alt="Ministerstwo Kultury i Dziedzictwa Narodowego. Dofinansowano ze środków Ministra Kultury i DziedzictwaNarodowego pochodzących z Funduszu Promocji Kultury – państwowego funduszu celowego"/>
+        <p>Dofinansowano ze środków Ministra Kultury i Dziedzictwa Narodowego pochodzących z Funduszu Promocji Kultury – państwowego funduszu celowego</p>
+        </div>
     </Container>
 }
