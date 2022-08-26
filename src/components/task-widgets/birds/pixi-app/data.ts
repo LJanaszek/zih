0e2c5634d1b4f5be6df1e168787a11b3307ea87b
Marek Borecki <borecki2@gmail.com>
2022-08-26 17:52:39 +0200

    pozycja ptaszków

--- src/components/task-widgets/birds/pixi-app/data.ts
+++ src/components/task-widgets/birds/pixi-app/data.ts
@@ -15,80 +15,80 @@ export const SCREEN: {
             id: 'b01',
             sprite: 'bird1',
             position: {
-                x: 100,
-                y: 100
+                x: 160,
+                y: 135
             }
         },
         {
             id: 'b02',
             sprite: 'bird2',
             position: {
-                x: 200,
-                y: 100
+                x: 610,
+                y: 76
             }
         },
         {
             id: 'b03',
             sprite: 'bird3',
             position: {
-                x: 100,
-                y: 200
+                x: 515,
+                y: 205
             }
         },
         {
             id: 'b04',
             sprite: 'bird4',
             position: {
-                x: 200,
-                y: 200
+                x: 650,
+                y: 195
             }
         },
         {
             id: 'b05',
-            sprite: 'bird5',
+            sprite: 'bird3',
             position: {
-                x: 300,
-                y: 100
+                x: 240,
+                y: 220
             }
         },
         {
             id: 'b06',
             sprite: 'bird6',
             position: {
-                x: 100,
-                y: 300
+                x: 135,
+                y: 390
             }
         },
         {
             id: 'b07',
             sprite: 'bird7',
             position: {
-                x: 300,
-                y: 300
+                x: 400,
+                y: 400
             }
         },
         {
             id: 'b08',
             sprite: 'bird8',
             position: {
-                x: 400,
-                y: 100
+                x: 135,
+                y: 495
             }
         },
         {
             id: 'b09',
             sprite: 'bird9',
             position: {
-                x: 100,
-                y: 400
+                x: 230,
+                y: 565
             }
         },
         {
             id: 'b10',
             sprite: 'bird10',
             position: {
-                x: 400,
-                y: 400
+                x: 520,
+                y: 545
             }
         },
     ]
