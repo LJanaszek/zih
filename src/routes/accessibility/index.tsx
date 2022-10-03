9dfdc6968411896f055231ce241fa55adc7867a9
Marek Borecki <borecki2@gmail.com>
2022-10-03 12:46:01 +0200

    dostepność

--- src/routes/accessibility/index.tsx
+++ src/routes/accessibility/index.tsx
@@ -13,11 +13,11 @@ export default function AccessibilityPage() {
             <Box>
                 <h2>Deklaracja dostepności</h2>
                 <p>
-                    Wawerskie Centrum Kultury  zobowiązuje się zapewnić dostępność swojej strony internetowej zgodnie z ustawą z dnia 4 kwietnia 2019 r. o dostępności stron internetowych i aplikacji mobilnych podmiotów publicznych. Oświadczenie w sprawie dostępności ma zastosowanie do strony internetowej  https://www.wck-wawer.pl.
+                    Wawerskie Centrum Kultury  zobowiązuje się zapewnić dostępność swojej strony internetowej zgodnie z ustawą z dnia 4 kwietnia 2019 r. o dostępności stron internetowych i aplikacji mobilnych podmiotów publicznych. Oświadczenie w sprawie dostępności ma zastosowanie do strony internetowej <a href='https://gra-z-schulzem.wck-wawer.pl/'>https://gra-z-schulzem.wck-wawer.pl/</a>.
                 </p>
                 <p>
-                    Data publikacji strony internetowej: 2022.09.05
-                    Data ostatniej aktualizacji strony internetowej: 2022.09.__<br />
+                    Data publikacji strony internetowej: 2022.09.30<br />
+                    Data ostatniej aktualizacji strony internetowej: 2022.09.30
                 </p>
                 <p className="before-list">
                     Strona internetowa jest częściowo zgodna z wytycznymi dostępności stron internetowych i aplikacji mobilnych z powodu niezgodności wymienionych poniżej:
@@ -32,11 +32,15 @@ export default function AccessibilityPage() {
                     Gra mobilna na stronie, nie jest zgodna z wytycznymi dostępności stron internetowych i aplikacji mobilnych. Brak dostępności wynika z:
                 </p>
                 <ul className="after-p">
-                    <li>geolokalizacyjnego charakteru gry, który wymaga od grającego aby ten dotarł do określonego punktu w terenie w celu uruchomienia gry i jej dalszych etapów</li>
-                    <li>zadania w grze wymagają użycia urządzenia mobilnego z ekranem dotykowym i aparatem</li>
+                    <li>
+                        geolokalizacyjnego charakteru gry, który wymaga od grającego aby ten dotarł do określonego punktu w terenie w celu uruchomienia gry i jej dalszych etapów
+                    </li>
+                    <li>
+                        zadania w grze wymagają użycia urządzenia mobilnego z ekranem dotykowym i aparatem
+                    </li>
                 </ul>
                 <p>
-                    Oświadczenie sporządzono dnia: ____<br />
+                    Oświadczenie sporządzono dnia: 30.09.2022<br />
                     Deklarację sporządzono na podstawie samooceny.
                 </p>
                 <h3>Skróty klawiszowe</h3>
@@ -45,8 +49,7 @@ export default function AccessibilityPage() {
                 </p>
                 <h3>Informacje zwrotne i dane kontaktowe</h3>
                 <p>
-                    W przypadku problemów z dostępnością strony internetowej prosimy o kontakt. Zgłoszenia kierować do Małgorzaty Basiak, mailowo - wawer.wck@um.warszawa.pl, telefonicznie – +48 22 443 70 73.<br />
-                    Tą samą drogą można składać wnioski o udostępnienie informacji niedostępnej oraz składać żądania zapewnienia dostępności.
+                    W przypadku problemów z dostępnością strony internetowej prosimy o kontakt. Zgłoszenia kierować do Małgorzaty Basiak, mailowo - wawer.wck@um.warszawa.pl, telefonicznie – +48 22 443 70 73.  Tą samą drogą można składać wnioski o udostępnienie informacji niedostępnej oraz składać żądania zapewnienia dostępności.
                 </p>
                 <p className="before-list">
                     W zgłoszeniu podaj:
@@ -54,7 +57,15 @@ export default function AccessibilityPage() {
                 <ul className="after-p">
                     <li>swoje imię i nazwisko,</li>
                     <li>swoje dane kontaktowe (np. numer telefonu, e-mail),</li>
+                    <li>dokładny adres strony internetowej, na której jest niedostępny element lub treść,</li>
+                    <li>opis na czym polega problem i jaki sposób jego rozwiązania byłby dla Ciebie najwygodniejszy.</li>
                 </ul>
+                <h3>
+                    Obsługa wniosków i skarg związanych z dostępnością
+                </h3>
+                <p>
+                    Każdy ma prawo do wystąpienia z żądaniem zapewnienia dostępności cyfrowej strony internetowej. Można także zażądać udostępnienia informacji za pomocą alternatywnego sposobu dostępu, na przykład przez odczytanie niedostępnego cyfrowo dokumentu, opisanie zawartości filmu bez audiodeskrypcji itp. Żądanie powinno zawierać dane osoby
+                </p>
             </Box>
             <div className="button-list">
                 <Link className="button" to={getHomeRoute()}>Powrót<i className="icon next" /></Link>
