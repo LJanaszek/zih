26468bd39b476830b21c64e404bbfc1ba69d0443
Marek Borecki <borecki2@gmail.com>
2022-10-07 14:25:21 +0200

    zwiększenie promienia punktu

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Z jakiej odległości można zaliczyć punkt
-    GEO_POINT_DISTANCE: 15, // m
+    GEO_POINT_DISTANCE: 20, // m
 
     // Minimalna dokładność GPS aby gra działała
     MIN_GEO_ACCURACY: 40, // m
