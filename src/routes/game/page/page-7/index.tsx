ed10eddbdf680b00a6739aef844ab76b1eb6c32f
skirk20 <lukasjanaszek@gmail.com>
2023-12-05 10:05:00 +0100

    zmiany

--- src/routes/game/page/page-7/index.tsx
+++ src/routes/game/page/page-7/index.tsx
@@ -1,48 +1,59 @@
+import { useState } from "react";
 import Box from "../../../../components/layout/box"
 import TextPage from "../../../../components/layout/text-page"
 
 type Props = {
     onNext(): void;
 };
+
 export default function Page1({ onNext }: Props) {
-    // let list = ["A","B","C","D","E","F"];
-    // document.getElementById('crypText')!.innerHTML = list[0];
+    const [setNext, next] = useState(false)
+    const [n, setN] = useState(0)
+    function crypUp() {
+        setN(n + 1)
+        if (n > 5 || n < 0) {
+            setN(0)
+        };
+        let list = ["A", "B", "C", "D", "E", "F"];
+        document.getElementById('crypText')!.innerHTML = list[n];
+        if (n === 3) {
+                next(true);
+            }else next(false);
+        console.log(n)
+    }
+
+    function crypDown() {
+        setN(n - 1)
+        if (n < 0 || n>5) {
+            setN(5)
+        };
+        let list = ["A", "B", "C", "D", "E", "F"];
+        document.getElementById('crypText')!.innerHTML = list[n];
+        if (n === 3) {
+            next(true);
+        }else next(false);
+        console.log(n)
+    }
+    
     return <TextPage>
         <Box>
             <h2>Cryptext</h2>
 
             <div className="">
-                <button onClick={()=>{crypUp()}}>arrow up</button>
-                    <h3 id="crypText">A</h3>
-                <button onClick={()=>{crypDown()}}>arrow down</button>
+                <button onClick={() => { crypUp() }}>arrow up</button>
+                <h3 id="crypText">A</h3>
+                <button onClick={() => { crypDown() }}>arrow down</button>
             </div>
-            
-
-
-
         </Box>
-        <div className="button-list">
-            <button className="button" onClick={onNext}>Przejdź do mapy<i className="icon map" /></button>
-        </div>
+        {setNext &&
+            <div className="button-list">
+                <button className="button" onClick={onNext}>Przejdź do mapy<i className="icon map" /></button>
+            </div>}
+        {(!setNext) &&<div className="button-list">
+            <button className="button">Przejdź<i className="icon map" /></button>
+        </div>}
     </TextPage>
-}
-var n = 0
-function crypUp(){
-    n++
-    if (n===5){
-        n = 0
-    };
-    let list = ["A","B","C","D","E","F"];
-    document.getElementById('crypText')!.innerHTML = list[n];
-    
+
 }
 
-function crypDown(){
-    n--
-    if (n<0){
-        n = 5
-    };
-    let list = ["A","B","C","D","E","F"];
-    document.getElementById('crypText')!.innerHTML = list[n];
-    
-}
+
